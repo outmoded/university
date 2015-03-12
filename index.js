4d71fc53345a0a58e7fac16f660dfaf34d8932e9
AdriVanHoudt <adri.van.houdt@gmail.com>
2015-03-12 10:38:26 +0100

    Basic server

---
+++ index.js
@@ -0,0 +1,31 @@
+var Hapi = require('hapi');
+
+var internals = {
+	pkg: require('./package.json')
+};
+
+var server = new Hapi.Server();
+
+server.connection({ port: process.env.PORT || 8000 });
+
+server.route({
+	method: 'GET',
+	path: '/version',
+	handler: function (request, reply) {
+
+		return reply({ version: internals.pkg.version });
+	},
+	config: {
+		description: 'Returns the version of the server'
+	}
+});
+
+server.start(function (err) {
+
+	if (err) {
+		throw err
+	}
+
+	// once we have something like good-console we can use server.log here
+	console.log('Server started at: ' + server.info.uri);
+});
