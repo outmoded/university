37c196e6e0d70ee8c7f29bf6f4dcfb3245983e70
Plamen Todorov <pl.todorov@gmail.com>
2015-03-15 23:47:24 +0200

    [Assignment 1 - Create a basic HTTP server]

---
+++ index.js
@@ -0,0 +1,18 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({
+  port: 8000
+});
+
+server.route({
+  method: 'GET',
+  path: '/version',
+  handler: function(request, reply) {
+    reply({ "version": require('./package.json').version });
+  }
+});
+
+server.start(function() {
+  console.log('Server running at: ' + server.info.uri);
+});
