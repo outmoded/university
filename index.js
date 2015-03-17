b610a518570593e143e195c2258d50ac2989930e
John Townsend <jtownsend@twitter.com>
2015-03-16 19:26:13 -0700

    initial commit

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ server.route({
   }
 });
 
+function count(x, limit) {
+  if( x > limit) return;
+  if( x % 3 == 0) {
+    if((x % 5 == 0) && (x % 2 != 0)) {
+      console.log(x);
+    }
+  }
+  count(x + 1, limit);
+}
+
+function outputNumbers(divisible, notDivisible) {
+  for(i=0; i< 100; i++) {
+    if((i % divisible) && !(i % notDivisible)) {
+      console.log(i);
+    } else {
+      console.log("skipping " + i);
+    }
+  }
+
+
+}
+
+
 server.start(function() {
+  count(1, 100);
   console.log("Server is running at: ", server.info.uri);
 });
