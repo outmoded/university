490e5473e4d563ff9d5ed6bc015e55d3eb330ef4
Roelof Wobben <r.wobben@home.nl>
2015-03-26 20:09:43 +0000

    first test have passed

--- test/version.js
+++ test/version.js
@@ -1,20 +1,34 @@
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
-var lib = require('../lib');
+// Load modules
+
 var Code = require('code');
+var Lab = require('lab');
+var Server = require('../lib');
 
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var describe = lab.experiment;
 var expect = Code.expect;
+var it = lab.test;
 
+lab.test('Give the version data of the package.json file ', function(done) {
 
-lab.test('Testing output on a valid url', function(done) {
-    
     var options = {
         method: 'GET',
         url: '/version'
-    };
-    
-    lib.init(function(err, server) {
-        
-        
-    });
+        };
+
+    Server.init(5000, function (err, server) {
+
+        server.inject(options, function(response) {
+
+            var result = response.result;
+
+            expect(response.statusCode).to.equal(200);
+            expect(result.version).to.equal('0.0.2');
+
+            done();
+            });
+        });
 });
