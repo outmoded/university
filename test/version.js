65f33fd73a4659150614f579b6f5821ee684817e
Roelof Wobben <r.wobben@home.nl>
2015-03-26 06:48:25 +0000

    again no working test

--- test/version.js
+++ test/version.js
@@ -1,39 +1,20 @@
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
-var server = require('../lib/index.js');
+var lib = require('../lib');
 var Code = require('code');
 
 var expect = Code.expect;
 
 
 lab.test('Testing output on a valid url', function(done) {
+    
     var options = {
         method: 'GET',
         url: '/version'
     };
-
-    server.inject(options, function(response) {
-        var result = response.result;
-
-        expect(response.statusCode).to.equal(200);
-        expect(result.version).to.equal('0.0.2');
-
-        done();
+    
+    lib.init(function(err, server) {
+        
+        
     });
 });
-
-lab.test('Testing output on a non - valid url', function(done) {
-    var options = {
-        method: 'GET',
-        url: '/wrong'
-    };
-
-    server.inject(options, function(response) {
-        var result = response.result;
-
-        expect(response.statusCode).to.equal(404);
-
-        done();
-    });
-});
-
