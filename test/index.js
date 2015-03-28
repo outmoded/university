1b684f968e345093fbfa374c3c7773fa780b378a
Fabriece Sumuni <sfabriece@hotmail.com>
2015-03-29 00:17:13 +0100

    first attempt

---
+++ test/index.js
@@ -0,0 +1,46 @@
+var Code = require('code');
+var Lab = require('lab');
+var Server = require('../lib');
+var Version = require('../lib/version');
+
+var expect = Code.expect;
+var labi = exports.lab = Lab.script();
+
+labi.experiment('index', function(){
+    labi.test('init starts the server', function(done){
+
+        Server.init(8081, function(err, server){
+            expect(err).to.be.undefined();
+            expect(server).to.be.an.object();
+            server.stop(done);
+        });
+    });
+
+    labi.test('no port supplied', function(done){
+
+        Server.init(function(err, server){
+
+            expect(server.info.port).to.be.equal(8000);
+            expect(err).to.be.undefined();
+            server.stop(done);
+        });
+    });
+
+    labi.test('should report error if plugin registration fails', function(done){
+
+        var correctVersionPlugin = Version.register;
+        var fakePluginError = 'Fake Plugin Error';
+        Version.register = function(server, options, next){
+
+            return next(fakePluginError);
+        };
+        Version.register.attributes = {name: 'Fake version Plugin' };
+
+        Server.init(8081, function(err, server){
+
+            Version.register = correctVersionPlugin;
+            expect(err).to.be.equal(fakePluginError);
+            server.stop(done);
+        });
+    });
+});
---
+++ test/version.js
@@ -0,0 +1,25 @@
+var Code = require('code');
+var Lab = require('lab');
+var Server = require('../lib');
+var Pkg = require('../package.json');
+
+var expect = Code.expect;
+var labi = exports.lab = Lab.script();
+
+labi.experiment('Version:', function(){
+    labi.test('returns correct version', function(done){
+
+        Server.init(8000, function(err, server){
+
+            var options = {
+                url: '/version'
+            };
+            server.inject(options, function(res){
+
+                expect(res.result).to.be.an.object();
+                expect(res.result.version).to.equal(Pkg.version);
+                server.stop(done);
+            });
+        });
+    });
+});
