c68714e6fb9b33ca343da7b9707b029a53cfb0cf
Rutaihwa <rutaihwa@gmail.com>
2015-03-29 22:25:11 +0300

    Dont start server when registration fails

--- test/index.js
+++ test/index.js
@@ -42,4 +42,38 @@ describe('lib', function() {
             server.stop(done);
         });
     });
+
+    it('Should not start a server when plugin registration fails', function (done) {
+
+	var register = Version.register;
+
+	Version.register = function (server, options, next) {
+
+            next('Errored');
+        };
+
+        Version.register.attributes = {
+            name: 'Random Name'
+        };
+
+        lib.init(function (err, server) {
+
+            expect(err).to.equal('Errored');
+            expect(server).to.not.exist();
+
+            Version.register = register;
+
+            done();
+        });
+    });
+
+    it('Should create server on a defined port, ' + internals.port, function (done) {
+
+        lib.init(internals.port, function(err, server) {
+
+            expect(err).to.be.undefined();
+            expect(server.info.port).to.equal(internals.port);
+            server.stop(done);
+        });
+    });
 });
