d973dab543f6069374e3b153f8601c14b68cc9a4
gyaresu <me@gareth.com.au>
2015-03-31 19:32:15 -0700

    writing tests

--- start.js
+++ start.js
@@ -1,14 +1,14 @@
 // Start.js - Why is this required over just index.js? 
-//
+// 
 var Hoek = require('hoek');
 
 // Requiring './lib' like this assigns the `register` & `init` methods to the variable `lib`. 
 
-var lib = require('./lib');
+var lib = require('./lib/index');
 
 // port and the callback are called 
 
-lib.init(3000, function (err, server) {
+lib.init(8000, function (err, server) {
 
     Hoek.assert(!err, err);
     console.log('Server started at: ' + server.info.uri);
--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 var Code = require('code');
 var Lab = require('lab');
+var Server = require('../lib/index');
+//var Version = require('../lib/version');
 
 var lab = exports.lab = Lab.script();
 var expect = Code.expect;
@@ -7,3 +9,16 @@ var describe = lab.experiment;
 var it = lab.test;
 
 
+describe('Should create a server with default port', function() {
+
+    it('starts the server', function(done) {
+
+        Server.init(8000, function(err, server) {
+
+            //expect(err).to.be.undefined();
+            //expect(server).to.be.an.object();
+            expect(server.info.port).to.equal(8000);
+            server.stop(done); // done is getting passed as a callback?
+        });
+    });
+});
