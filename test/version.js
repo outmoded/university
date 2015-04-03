ce06097d062bea475716815affd65c6a34493b3e
Fabriece Sumuni <sfabriece@hotmail.com>
2015-04-03 12:34:10 +0200

    changed test to test for deep equality

--- test/version.js
+++ test/version.js
@@ -6,20 +6,20 @@ var Pkg = require('../package.json');
 var expect = Code.expect;
 var labi = exports.lab = Lab.script();
 
-labi.experiment('Version:', function(){
-    labi.test('returns correct version', function(done){
+labi.experiment('Version:', function() {
+	labi.test('returns correct version', function(done) {
 
-        Server.init(8000, function(err, server){
+		Server.init(8000, function(err, server) {
 
-            var options = {
-                url: '/version'
-            };
-            server.inject(options, function(res){
+			var options = {
+				url: '/version'
+			};
+			server.inject(options, function(res) {
 
-                expect(res.result).to.be.an.object();
-                expect(res.result.version).to.equal(Pkg.version);
-                server.stop(done);
-            });
-        });
-    });
+				expect(res.result).to.be.an.object();
+				expect(res.result.version).to.deep.equal(Pkg.version);
+				server.stop(done);
+			});
+		});
+	});
 });
