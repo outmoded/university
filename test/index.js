ef2d66dcbd29fd2cf656634793499fb040923c30
Jon Swenson <jswenson74@gmail.com>
2015-04-15 19:47:27 -0400

    Clean up based on @hueniverse general comments.

--- test/index.js
+++ test/index.js
@@ -26,14 +26,35 @@ describe('Check server startup',function(){
     });
 
 
-    it('test version plugin load fail', function(done){
+    it('test server missing port argument', function(done){
+
+        Lib.init(function(err, server){
+
+            expect(err.message).to.equal('Error no port submitted');
+            server.stop(done);
+        });
+    });
+
+
+    it('test server port is not valid number', function(done){
+
+        Lib.init('port not number', function(err, server){
+
+            expect(err.message).to.equal('Error port is not valid number');
+            server.stop(done);
+        });
+    });
+
+
+    it('test version plugin load fail', { parallel: false }, function(done){
 
         var register = Version.register;
 
         // Credit: Break plugin code based on  @TheAlphaNerd's assignment3.
         Version.register = function (server, options, next) {
 
-            next('Break plugin');
+            // next('Break plugin');
+            return next(new Error('Boom Error'));
         };
 
 
@@ -44,7 +65,7 @@ describe('Check server startup',function(){
 
         Lib.init(0, function(err, server){
         
-            expect(err).to.equal('Break plugin');
+            expect(err.message).to.equal('Boom Error');
             Version.register = register;
             server.stop(done);
         });
