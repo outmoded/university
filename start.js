f6eb266ad5e1e54cf8be81237008fd136f148d7c
Roelof Wobben <r.wobben@home.nl>
2015-04-16 15:33:19 +0000

    solved error when running on lib.

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ var Hoek = require('hoek');
 
 var Lib = require('./lib');
 
-lib.init(8000, function (err, server) {
+Lib.init(8000, function (err, server) {
 
     Hoek.assert(!err, err);
     console.log('Server started at: ' + server.info.uri);
