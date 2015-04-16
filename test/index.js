cee0e117004cb2dfc463e7699dac6e11a3363a0f
Jon Swenson <jswenson74@gmail.com>
2015-04-15 20:18:52 -0400

    Cleaned up the init() and related tests.

--- test/index.js
+++ test/index.js
@@ -26,26 +26,6 @@ describe('Check server startup',function(){
     });
 
 
-    it('test server missing port argument', function(done){
-
-        Lib.init(function(err, server){
-
-            expect(err.message).to.equal('Error no port submitted');
-            server.stop(done);
-        });
-    });
-
-
-    it('test server port is not valid number', function(done){
-
-        Lib.init('port not number', function(err, server){
-
-            expect(err.message).to.equal('Error port is not valid number');
-            server.stop(done);
-        });
-    });
-
-
     it('test version plugin load fail', { parallel: false }, function(done){
 
         var register = Version.register;
@@ -53,7 +33,6 @@ describe('Check server startup',function(){
         // Credit: Break plugin code based on  @TheAlphaNerd's assignment3.
         Version.register = function (server, options, next) {
 
-            // next('Break plugin');
             return next(new Error('Boom Error'));
         };
 
