056696bbae5eba293eca9fa4fdab5d19092098e4
Frank Rutaihwa <rutaihwa@gmail.com>
2015-04-17 22:48:28 +0300

    Changes due to general feedback

--- test/index.js
+++ test/index.js
@@ -2,13 +2,12 @@
 
 var Code = require('code');
 var Lab = require('lab');
-var lib = require('../lib');
+var index = require('../lib/index');
 var Version = require('../lib/version');
 
 // Declare internals
 
 var internals = {
-    defaultPort: 8000,
     port: 8001
 };
 
@@ -21,21 +20,22 @@ var expect = Code.expect;
 
 // Test lib
 
-describe('lib', function() {
+describe('index', function() {
 
-    it('Should create server on default port, ' + internals.defaultPort, function (done) {
+    it('Should just create a server', function (done) {
 
-        lib.init(function (err, server) {
+        return index.init(0, function (err, server) {
 
             expect(err).to.be.undefined();
-            expect(server.info.port).to.equal(internals.defaultPort);
+            expect(server).to.be.an.object();
+
             server.stop(done);
         });
     });
 
     it('Should create server on a defined port, ' + internals.port, function (done) {
 
-        lib.init(internals.port, function(err, server) {
+        return index.init(internals.port, function(err, server) {
 
             expect(err).to.be.undefined();
             expect(server.info.port).to.equal(internals.port);
@@ -43,37 +43,28 @@ describe('lib', function() {
         });
     });
 
-    it('Should not start a server when plugin registration fails', function (done) {
+    it('Should not start a server when plugin registration fails', { parallel: false }, function (done) {
 
 	var register = Version.register;
 
 	Version.register = function (server, options, next) {
 
-            next('Errored');
+            return next(new Error('Error'));
         };
 
         Version.register.attributes = {
             name: 'Random Name'
         };
 
-        lib.init(function (err, server) {
+        return index.init(0, function (err, server) {
 
-            expect(err).to.equal('Errored');
+            expect(err, 'error').to.be.an.instanceOf(Error);
+            expect(err.message, 'error message').to.be.equal('Error');
             expect(server).to.not.exist();
 
             Version.register = register;
 
             done();
-        });
-    });
-
-    it('Should create server on a defined port, ' + internals.port, function (done) {
-
-        lib.init(internals.port, function(err, server) {
-
-            expect(err).to.be.undefined();
-            expect(server.info.port).to.equal(internals.port);
-            server.stop(done);
         });
     });
 });
--- test/version.js
+++ test/version.js
@@ -2,7 +2,7 @@
 
 var Code = require('code');
 var Lab = require('lab');
-var lib = require('../lib');
+var index = require('../lib/index');
 var Pkg = require('../package.json');
 
 // Lab shortcuts
@@ -13,16 +13,17 @@ var it = lab.it;
 var expect = Code.expect;
 
 describe('version', function() {
-    it('should return version details without errors', function (done) {
+    it('When GET /version server should return version details', function (done) {
 
-	lib.init(function (err, server) {
+	return index.init(0, function (err, server) {
 
             expect(err).to.be.undefined();
-            expect(server.info.port).to.equal(8000);
-            server.inject('/version', function (response) {
+
+            return server.inject('/version', function (response) {
 
                 expect(response.statusCode).to.equal(200);
                 expect(response.result.version).to.equal(Pkg.version);
+
                 server.stop(done);
             });
 	});
