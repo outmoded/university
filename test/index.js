71c0e5ffad14f7e089b2a6be8bd9806b98e2c36f
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2015-04-18 15:38:30 +0300

    Fix tests to cover 100%

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@
 var Code = require('code');
 var Lab = require('lab');
 var Server = require('../lib');
+var Version = require('../lib/version');
 
 
 // Test shortcuts
@@ -17,7 +18,7 @@ describe('server init', function () {
 
     it('should start server on given port', function (done) {
 
-        Server.init(8080, function (err, server){
+        Server.init(8080, function (err, server) {
 
             expect(server.info.port).to.equal(8080);
             expect(err).to.be.undefined();
@@ -27,7 +28,7 @@ describe('server init', function () {
 
     it('should start server on 8000 if no port given', function (done) {
 
-        Server.init(function (err, server){
+        Server.init(function (err, server) {
 
             expect(server.info.port).to.equal(8000);
             expect(err).to.be.undefined();
@@ -37,10 +38,31 @@ describe('server init', function () {
 
     it('should start server on 8000 if null given', function (done) {
 
-        Server.init(null, function (err, server){
+        Server.init(null, function (err, server) {
 
             expect(server.info.port).to.equal(8000);
             expect(err).to.be.undefined();
+            server.stop(done);
+        });
+    });
+
+    it('should give error if plugin loading fails', function (done) {
+
+        var register = Version.register;
+
+        Version.register = function (server, options, next) {
+
+            return next(new Error('foo error'));
+        };
+
+        Version.register.attributes = {
+            name: 'foo'
+        };
+
+        Server.init(8080, function (err, server) {
+
+            expect(err.message).to.equal('foo error');
+            Version.register = register;
             server.stop(done);
         });
     });
