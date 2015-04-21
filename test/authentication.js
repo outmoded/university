efaf6059eae72eadc106a33ec88f68d21bdae3ab
Roelof Wobben <r.wobben@home.nl>
2015-04-21 07:21:35 +0000

    no coverage on line 13 lib/authentication.js

--- test/authentication.js
+++ test/authentication.js
@@ -17,22 +17,74 @@ var expect = Code.expect;
 var it = lab.test;
 
 it('returns a greeting for the authenticated user', function (done) {
-    
+
     Hueniversity.init(0, function (err, server) {
-        
+
         expect(err).to.not.exist();
-        
+
         var request = { method: 'GET', url: '/private', headers: { authorization: internals.header(Users.user1.username, Users.user1.password) } };
         server.inject(request, function (res) {
-            
+
             expect(res.statusCode, 'Status code').to.equal(200);
-            expect(res.result, 'result').to.equal('<div>Hello Foo</div>');
+            expect(res.result, 'result').to.equal('<div>Hello user1</div>');
 
             server.stop(done);
             });
         });
 });
 
+it('returns a error message  for the authenticated user with a wrong password', function (done) {
+
+    Hueniversity.init(0, function (err, server) {
+
+        expect(err).to.not.exist();
+
+        var request = { method: 'GET', url: '/private', headers: { authorization: internals.header(Users.user1.username, 'wrong password') } };
+        server.inject(request, function (res) {
+
+            expect(res.statusCode, 'Status code').to.equal(401);
+
+            server.stop(done);
+            });
+        });
+});
+
+it('returns a error message  for the authenticated user with a wrong password', function (done) {
+
+    Hueniversity.init(0, function (err, server) {
+
+        expect(err).to.not.exist();
+
+        var request = { method: 'GET', url: '/private', headers: { authorization: internals.header('', '') } };
+        server.inject(request, function (res) {
+
+            expect(res.statusCode, 'Status code').to.equal(401);
+
+            server.stop(done);
+            });
+        });
+});
+
+it('returns error on failed registering of auth', { parallel: false }, function (done) {
+
+        var orig = Basic.register;
+        Basic.register = function (plugin, options, next) {
+
+            return next(new Error('fail'));
+        };
+        Basic.register.attributes = {
+            name: 'fake hapi-auth-basic'
+        };
+
+        Hueniversity.init(0, function (err) {
+
+            Basic.register = orig;
+
+            expect(err).to.exist();
+
+            done();
+        });
+});
 
 internals.header = function (username, password) {
 
