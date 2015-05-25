574df024527901d29ec8f3c46eb524faabf1ade7
roelof1967 <rwobben@hotmail.com>
2015-05-25 09:54:18 +0000

    take another road to solve the tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,15 @@
+// Load modules
+
 var Hapi = require('hapi');
 var Code = require('code');
 var Lab = require('lab');
-var Composer = require('../lib/start');
+var University = require('../lib');
+var Version = require('../lib/version');
+var Path = require('path');
+
+//declare internals
+
+var internals = {};
 
 // Test shortcuts
 
@@ -9,14 +17,18 @@ var lab = exports.lab = Lab.script();
 var expect = Code.expect;
 var it = lab.test;
 
+
 it('starts server and returns hapi server object', function (done) {
 
-    Composer(function (err, composedServer) {
+    University.init({}, {}, function (err, server) {
 
-            expect(composedServer).to.be.an.object();
-            done();
-        });
-}); 
+        expect(err).to.not.exist();
+        expect(server).to.be.instanceof(Hapi.Server);
+
+        server.stop(done);
+    });
+});
+
 
 
                        
