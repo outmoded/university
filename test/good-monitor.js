36ff27d790f4d7732bef8df5565fad69dba042e3
Jon Swenson <jswenson74@gmail.com>
2015-07-13 19:31:32 +0800

    Added good monitoring
    * 100% coverage
      But, used a hack to get coverage.
      see: lib/good-monitor.js for the hack.

---
+++ test/good-monitor.js
@@ -0,0 +1,68 @@
+var Code = require('code');
+var Lab = require('lab');
+var University = require('../lib');
+var Path = require('path');
+var Config = require('../lib/config');
+var Good = require('good');
+var GoodMonitor = require('../lib/good-monitor');
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var describe = lab.experiment;
+var expect = Code.expect;
+var it = lab.test;
+
+
+describe('good-monitor', function () {
+
+    it('monitor registration failed', { parallel: false }, function (done) {
+
+        var orig = Good.register;
+
+        Good.register = function (plugin, options, next) {
+
+            Good.register = orig;
+            return next(new Error('fail'));
+        };
+
+        Good.register.attributes = {
+            name: 'fake good'
+        };
+
+        University.init(internals.manifest, internals.composeOptions, function (err, server) {
+
+            expect(err).to.exist();
+            expect(err.message).to.equal('fail');
+            done();
+        });
+    });
+});
+
+internals.manifest = {
+    connections: [
+        {
+            host: 'localhost',
+            port: 0,
+            labels: ['web']
+        },
+        {
+            host: 'localhost',
+            port: 0,
+            labels: ['web-tls', 'api'],
+            tls: Config.tls
+        }
+    ],
+    plugins: {
+        './good-monitor': {}
+    }
+};
+
+internals.composeOptions = {
+    relativeTo: Path.resolve(__dirname, '../lib')
+};
