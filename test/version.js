a52e70002610d6faf34f41c85e097107f3bbd263
despirados <ayed.hedi@gmail.com>
2016-04-07 14:58:50 +0200

    First Commit, tests not yet working

---
+++ test/version.js
@@ -0,0 +1,16 @@
+const Version = require('../lib/version');
+const Code = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+describe('version', () => {
+
+    it('returns a correct verion content', (done) => {
+
+        done();
+    });
+});
