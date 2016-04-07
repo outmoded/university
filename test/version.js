106d77d5fd5a6784321b2c9cace46f4b3a4c87b7
despirados <ayed.hedi@gmail.com>
2016-04-07 16:02:32 +0200

    Assignment3 works now

--- test/version.js
+++ test/version.js
@@ -1,20 +1,29 @@
-const Init = require('../lib/index');
+const Univesity = require('../lib');
+const Package = require('../package.json');
 const Code = require('code');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
+
 
 const describe = lab.describe;
 const it = lab.it;
 const expect = Code.expect;
 
-describe('version', () => {
+describe('/version', () => {
 
     it('returns a correct verion from pakage.json', (done) => {
 
-        Init(0, (err, server) => {
+        Univesity.init(0, (err, server) => {
 
+            expect(err).to.not.exist();
+
+            server.inject('/version', (res) => {
+
+                expect(res.statusCode).to.equal(200);
+                expect(res.result).to.deep.equal({ version: Package.version });
+
+                server.stop(done);
+            });
         });
-
-        done();
     });
 });
