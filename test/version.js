25d676869287ca5d1ec1716994db9c6713fa1f0b
Jon Swenson <jswenson74@gmail.com>
2018-12-22 17:36:09 -0500

    lesson3 100% test coverage

---
+++ test/version.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// Credits: Script based off work by
+// @idanwe https://github.com/idanwe
+// See this PR: https://github.com/hapijs/university/pull/85
+
+const Lab = require('lab');
+const Code = require('code');
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const describe = lab.experiment;
+const expect = Code.expect;
+const it = lab.test;
+
+const University = require('../lib');
+
+const internals = {};
+
+internals.serverConfigs = {
+    server: {
+        port: 0
+    }
+};
+
+
+describe('/version', () => {
+
+    it('/version success', { parallel: false }, async () => {
+
+        const server = await University.init(internals.serverConfigs);
+
+        expect(server).to.be.an.object();
+
+        const res = await server.inject('/version');
+
+        expect(res.result.message).to.equal('option passed to version plugin');
+        expect(res.result.version).to.equal('1.0.3');
+
+        await server.stop();
+    });
+});
